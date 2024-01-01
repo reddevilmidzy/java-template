ff38d487152f4d7f1c3ad50c221abd3297404080
CHOI DONGGEUN <78539407+reddevilmidzy@users.noreply.github.com>
2024-01-02 01:39:06 +0900

    Delete OutputView.java

--- OutputView.java
+++
@@ -1,8 +0,0 @@
-public class OutputView {
-  
-    public static final String ERROR_FORM = "[ERROR] %s%n";
-
-    public void printErrorMessage(Throwable throwable) {
-        System.out.printf(ERROR_FORM, throwable.getMessage());
-    }
-}
